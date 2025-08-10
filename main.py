13a96d0a99761508cd6f5c5bc0e6af3f59c2b217
Bayu Wicaksono <bwbayuuu@gmail.com>
2025-08-10 21:39:14 +0700

    feat: add create corpus, train bm25 model, and generate sparse vector

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ HOST_PINECONE_SPARSE = os.getenv('HOST_PINECONE_SPARSE')
 SILICONFLOW_URL = os.getenv('SILICONFLOW_URL')
 SILICONFLOW_API_KEY = os.getenv('SILICONFLOW_API_KEY')
 NAMESPACE = os.getenv('NAMESPACE')
-EMBED_DIM = os.getenv('EMBED_DIM')
+EMBED_DIM = int(os.getenv('EMBED_DIM')) if os.getenv('EMBED_DIM') else None
 print("load key done")
 
 # config
@@ -26,7 +26,27 @@ headers = {
 }
 print("config done")
 
-def get_embeddings(text, dim_size = 1024):
+def create_corpus(corpus, folder_path):
+    if os.path.isdir(folder_path):
+        for filename in os.listdir(folder_path):
+            if len(filename.split('.')) == 2 and filename.split('.')[1] == 'json':
+                file_path=folder_path+'/'+filename
+                try:
+                    with open(file_path, 'r') as file:
+                        data = json.load(file)
+                        for item in data:
+                            corpus.append(item['text'])
+                        
+                except FileNotFoundError:
+                    print(f"Error: {file_path} not found. Please ensure the file exists in the correct directory.")
+                except json.JSONDecodeError:
+                    print(f"Error: Could not decode JSON from {file_path}. The file might be malformed.")
+                except Exception as e:
+                    print(f"An unexpected error occurred: {e}")
+    
+    print("corpus created successfully")
+
+def get_dense_embeddings(text, dim_size = 1024):
     payload = {
         "model": "Qwen/Qwen3-Embedding-8B",
         "input": text,
@@ -38,20 +58,32 @@ def get_embeddings(text, dim_size = 1024):
 
     return response.json()['data'][0]['embedding']
 
-def generate_embedding(path_files):
+def get_sparse_embeddings(text, bm25_model):
+    return bm25_model.encode_documents(text)
+
+def generate_embedding(path_files, bm25_model):
     try:
         with open(path_files, 'r') as file:
             data = json.load(file)
-            new_data = []
+            dense_vectors = []
+            sparse_vectors = []
             for item in data:
-                new_item = {
+                # get dense embedding
+                dense_item = {
                     "id": item['_id'], 
-                    "values": get_embeddings(item['text'], EMBED_DIM), 
+                    "values": get_dense_embeddings(item['text'], EMBED_DIM), 
                     "metadata": {key: value for key, value in item.items() if key not in {'_id'}}
                 }
-                new_data.append(new_item)
+                dense_vectors.append(dense_item)
+                # get sparse embedding
+                sparse_item = {
+                    "id": item['_id'], 
+                    "sparse_values": get_sparse_embeddings(item['text'], bm25_model), 
+                    "metadata": {key: value for key, value in item.items() if key not in {'_id'}}
+                }
+                sparse_vectors.append(sparse_item)
             
-            return new_data
+            return dense_vectors, sparse_vectors
     except FileNotFoundError:
         print(f"Error: {path_files} not found. Please ensure the file exists in the correct directory.")
     except json.JSONDecodeError:
@@ -59,7 +91,31 @@ def generate_embedding(path_files):
     except Exception as e:
         print(f"An unexpected error occurred: {e}")
 
-index_dense.upsert(
-  vectors=generate_embedding("data/full_text.json"),
-  namespace=NAMESPACE
-)
+# read data
+folder_path = 'data/clean'
+
+# define bm25 model
+bm25 = BM25Encoder(stem=False)
+# create bm25 corpus for sparse vector
+bm25_corpus = []
+create_corpus(bm25_corpus, folder_path)
+
+# fit corpus to bm25 model
+bm25.fit(bm25_corpus)
+
+# generate dense and sparse vector
+if os.path.isdir(folder_path):
+    for filename in os.listdir(folder_path):
+        if len(filename.split('.')) == 2 and filename.split('.')[1] == 'json':
+            file_path=folder_path+'/'+filename
+            # insert data dense
+            dense_vectors, sparse_vectors = generate_embedding(file_path, bm25_model=bm25)
+            index_dense.upsert(
+                namespace=NAMESPACE,
+                vectors=dense_vectors
+            )
+            # insert data sparse
+            index_sparse.upsert(
+                namespace=NAMESPACE,
+                vectors=sparse_vectors
+            )
